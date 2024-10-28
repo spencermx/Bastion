a587e820163558ae2bbfcb22a1a44f0b151a2e75
spencermx <maas.spencer@outlook.com>
2024-10-28 17:23:24 -0600

    updated name

--- bastion_ssr/components/Navbar.jsx
+++ bastion_ssr/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            spencermaas.com
           </span>
         </a>
         <button
